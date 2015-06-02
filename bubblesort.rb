f75cc366c241aa79553bf84a7ca5b71d70e1a79e
bhupendra11 <bhupendrashekhawat1108@gmail.com>
2015-06-02 11:26:49 +0530

    bubble_sort_by added

--- bubblesort.rb
+++ bubblesort.rb
@@ -26,3 +26,25 @@ end
 
 
 puts bubble_sort [4,3,78,2,0,2]
+
+
+
+####################################   bubble_sort_by ######################
+
+def bubble_sort_by(array) 
+  counter = array.size - 1
+
+  while counter > 0
+    1.upto(counter) do |number|
+      if yield(array[number - 1], array[number]) < 0
+        array[number - 1], array[number] = array[number], array[number - 1]
+      end
+    end
+    counter -= 1
+  end
+
+  return array
+end
+
+
+puts bubble_sort_by(["hi","hello","hey"]) { |left, right| right.length - left.length }
